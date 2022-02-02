fcd1b4569bae6355ed05ad2db299bd70383ea12d
Leonardorossato <leonardorossato21@gmail.com>
2022-02-02 11:26:33 -0300

    import da rota task

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const tasksRouter = require('./routes/tasksRouter')
 const mongoConnection = require('./config/mongoConnection')
 require('dotenv').config
 const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/api', tasksRouter)
+
 app.mongoConnection = mongoConnection
 
 app.listen(PORT, ()=>{
