cfcfdf0682bc13332f9c30ad0daf5f3ea3963be9
Leonardorossato <leonardorossato21@gmail.com>
2022-02-02 10:02:02 -0300

    ajuste no import da conexão

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const mongoConnection = require('./config/mongoConnection')
 const PORT = 5000 || process.env.PORT
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.mongoConnection = mongoConnection
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on the port : ${PORT}`)
