28c9910e25238488f6dfb8f840036cf59f4f1abd
Leonardorossato <leonardorossato21@gmail.com>
2022-02-03 13:47:07 -0300

    min and max

--- models/Users.js
+++ models/Users.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.Schema({
+const userSchema =new mongoose.Schema({
     name : {type: String, required: true},
-    email : {type: String , required: true, unique: true},
-    password : {type: String, required: true}
+    email : {type: String , required: true, unique: true, min: 10, max: 200},
+    password : {type: String, required: true, min: 6, max: 50}
 })
 
 const Users = mongoose.model('users', userSchema)
