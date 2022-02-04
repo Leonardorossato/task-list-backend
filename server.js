adfc16d2d9cef5dfb53911b05e0649f235a074e3
Leonardorossato <leonardorossato21@gmail.com>
2022-02-04 11:01:27 -0300

    comentarios add

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
+//imports of the package.json
 require('rootpath')()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+require('dotenv').config()
+const PORT = process.env.PORT
+/**/
+
+//Imports of the routes and controllers
 const tasksRouter = require('./routes/tasksRouter')
 const basicAuth = require('./helpers/basicAuth')
 const erroHandler = require('./helpers/erroHandler')
 const userController = require('./Controllers/usersController')
-const mongoConnection = require('./config/mongoConnection')
-require('dotenv').config()
-const PORT = process.env.PORT
+//
 
+// variable of mongoDB connection
+const mongoConnection = require('./config/mongoConnection')
+
+//Using middleware
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors())
+//
 
 app.use(basicAuth);
 
+//routes
 app.use('/api', tasksRouter)
 app.use('/api', userController)
+//
 
 app.use(erroHandler)
 
+//Using mongo connectiom
 app.mongoConnection = mongoConnection
+//
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on the port : ${PORT}`)
