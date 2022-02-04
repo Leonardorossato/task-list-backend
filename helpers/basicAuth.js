1691eab5b61d3a14e34b3b14d27873a4c43b4315
Leonardorossato <leonardorossato21@gmail.com>
2022-02-04 11:01:39 -0300

    erro na varival ajustada

--- helpers/basicAuth.js
+++ helpers/basicAuth.js
@@ -1,18 +1,19 @@
 const userService = require('../Controllers/userService')
 
-const basicAuth = async(req,res, next) => {
+async function basicAuth (req,res, next)  {
+    const base64Credentials =  req.headers.authorization.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+    const user = await userService.authenticate({ username, password });
+    
     if(req.path === '/api/authenticate'){
         return next()
     }
 
-    if(!req.headers.autorization || req.headers.autorization.indexOf('Basic ')=== -1){
+    if(!req.headers.authorization || req.headers.authorization.indexOf('Basic ')=== -1){
         return res.status(401).json({message : 'Missing Authorization Header'})
     }
 
-    const base64Credentials =  req.headers.authorization.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-    const [name, password] = credentials.split(':');
-    const user = await userService.authenticate({ name, password });
     if (!user) {
         return res.status(401).json({ message: 'Invalid Authentication Credentials' });
     }
