41e363aef682f46930d24f4ffb74a41ec067715f
Leonardorossato <leonardorossato21@gmail.com>
2022-02-06 11:17:06 -0300

    fazendo as modificações

--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -2,31 +2,22 @@ const express = require('express');
 const Tasks = require('../models/Tasks')
 const router = express.Router()
 
-router.get('/', (req, res)=>{
-    Tasks.find((err, data) => {
-        if(err){
-            res.status(500).json('Error to get all the tasks', err)
-        }
-        return res.status(200).json(data)
-    })  
+router.get('/', async(req, res)=>{
+    try {
+        const tasks = await Tasks.findAll()
+        res.status(200).json(tasks)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 });
 
-router.post('/tasks', (req, res)=>{
-    let title = req.body.title
-    let completed = req.body.completed
-    let editing = req.body.editing
-
-    let newTask = new Tasks({
-        title,
-        completed,
-        editing
-    })
-
-    newTask.save().then(()=>{
-        res.status(201).json(newTask)
-    }).catch((err)=>{
-        res.status(400).json('Error to add the tasks'+err)
-    })
+router.post('/tasks', async(req, res)=>{
+    try {
+        const tasks = await Tasks.create(req.body)
+        res.status(201).json(tasks)
+    }catch (error) {
+        res.status(400).json(error)
+    }
 })
 
 router.put('/tasks/:id', async(req, res)=>{
