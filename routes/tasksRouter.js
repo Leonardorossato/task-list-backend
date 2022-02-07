685ae92f60fb5354f72a7628361b0c954c64ab91
Leonardorossato <leonardorossato21@gmail.com>
2022-02-07 11:42:05 -0300

    so falta arrumar a rota de atualizar

--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const Tasks = require('../models/Tasks')
 const router = express.Router()
+const Tasks = require('../models/Tasks')
 
+//get all tasks
 router.get('/', async(req, res)=>{
     try {
         const tasks = await Tasks.findAll()
@@ -20,27 +21,22 @@ router.post('/tasks', async(req, res)=>{
     }
 })
 
-router.put('/tasks/:id', async(req, res)=>{
-    try {
-        const tasksId = req.params.id;
-        const tasks = await Tasks.findOne({where: {id: tasksId}})
-        tasks.title = req.body.title
-        tasks.completed = req.body.completed
-        tasks.editing = req.body.editing
-        await tasks.save()
-        res.status(200).json(tasks)
-    } catch (error) {
-        res.status(500).json(error)    
-    }
+router.put('/tasks/:id', async (req, res)=>{
+    const taskId = req.params.id;
+    const task = await Tasks.findOne({where : {id : taskId}});
+    
+    await task.save()
+    res.status(200).json('ok')
+    
 })
 
 router.delete('/tasks/:id', async(req, res) => {
-    try {
-        await Tasks.findOneAndRemove(req.params.id)
-        res.status(200).json('Task deleted successfully')
-    }catch (err) {
-        res.status(500).json(err)
-    }
+    const taskId = req.params.id
+    await Tasks.destroy({where: {id: taskId}}).then(()=>{
+        res.status(200).json('Task deleted sucessfully')
+    }).catch((error)=>{
+        res.status(500).json(error)
+    })
 })
 
 module.exports = router
