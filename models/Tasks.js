606d40e9dc971da87fdb4e6296bfc4e7208a3e7d
Leonardorossato <leonardorossato21@gmail.com>
2022-02-08 11:00:21 -0300

    ok

--- models/Tasks.js
+++ models/Tasks.js
@@ -4,12 +4,6 @@ const sequelize = require('../config/db')
 class Tasks extends Model {}
 
 Tasks.init({
-    id:{
-       type: DataTypes.INTEGER,
-       primaryKey: true,
-       autoIncrement: true,
-       allowNull: false
-    },
     title:{
         type: DataTypes.STRING
     },
