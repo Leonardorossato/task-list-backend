712f23687e3bd8a4bd95a833ef877f8f3b765e9d
Leonardorossato <leonardorossato21@gmail.com>
2022-02-11 11:22:21 -0300

    ok

--- helpers/basicAuth.js
+++ helpers/basicAuth.js
@@ -2,15 +2,16 @@ const userService = require('../Controllers/userService')
 
 async function basicAuth (req,res, next)  {
 
-    const base64Credentials =  req.headers.authorization.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-    const [username, password] = credentials.split(':');
-    const user = await userService.authenticate({ username, password });
     // attach user to request object
 
     if(!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1){
         return res.status(401).json({message : 'Missing Authorization Header'})
     }
+
+    const base64Credentials =  req.headers.authorization.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+    const user = await userService.authenticate({ username, password });
 
     if (!user) {
         return res.status(401).json({ message: 'Invalid Authentication Credentials' });
